22b65d905f08d4195c83bfc80ce2d1c07349e0c5
Luke Brierley <ldbrierley@gmail.com>
2018-12-18 18:44:10 +0000

    added a new scenario

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,5 @@ fn main() {
         2 => println!("Lets go!"),
         _ => unreachable!(),
     }
-    println!("He undid the handcuffs on you slowly, you were in range Dr Lamption had tricked you and locked you up in this prison just to torture your very own soul.\n He knew, he knew the very power you could posess, he knew what happend, he knew everything even Andrew.\n Andrew took you to the exit of the prison when two gaurds stoped you  ")
+    println!("He undid the handcuffs on you slowly, you were in range Dr Lamption had tricked you and locked you up in this prison just to torture your very own soul.\n He knew.\n He knew the very power you could posess, he knew what happend, he knew everything even Andrew.\n Andrew took you to the exit of the prison when two gaurds stoped you.\n Did you.\n 1) Leave andrew to do all the talking.\n 2) Tell the guards you are visitor and a friend of Andrew.\n 3) Laugh at the guards.\n 4) Attack them.\n 5) Ruuuuuunn!!!")
 }
